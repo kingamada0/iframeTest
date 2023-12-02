8aad0b51e53f70d2cbbfd0e8ac06eb7fa20c8532
kingamada0 <152681071+kingamada0@users.noreply.github.com>
2023-12-02 07:14:20 -0800

    Update fetchContent.js

--- api/fetchContent.js
+++ api/fetchContent.js
@@ -1,40 +1,42 @@
 const https = require('https');
-const { JSDOM } = require('jsdom'); // Ensure JSDOM is included in your dependencies
+const { JSDOM } = require('jsdom');  // Ensure JSDOM is installed
 
 module.exports = async (req, res) => {
     const targetUrl = 'https://lordne.vercel.app/';
 
     https.get(targetUrl, (response) => {
-        let data = '';
+        let rawData = '';
 
         response.on('data', (chunk) => {
-            data += chunk;
+            rawData += chunk;
         });
 
         response.on('end', () => {
             try {
-                const dom = new JSDOM(data);
+                const dom = new JSDOM(rawData);
                 const document = dom.window.document;
 
-                // Function to check if a URL is absolute
-                const isAbsoluteURL = (url) => /^(?:[a-z]+:)?\/\//i.test(url);
+                // Process all <a>, <link>, <script>, and <img> elements
+                const elements = document.querySelectorAll('a[href], link[href], script[src], img[src]');
+                elements.forEach(el => {
+                    const attr = el.hasAttribute('href') ? 'href' : 'src';
+                    let url = el.getAttribute(attr);
 
-                // Update href/src attributes
-                document.querySelectorAll('link[href], script[src], img[src]').forEach(element => {
-                    const url = element.getAttribute('href') || element.getAttribute('src');
-                    if (url && !isAbsoluteURL(url)) {
-                        const newUrl = new URL(url, targetUrl).href; // Construct absolute URL
-                        if (element.tagName === 'LINK') element.setAttribute('href', newUrl);
-                        if (element.tagName === 'SCRIPT' || element.tagName === 'IMG') element.setAttribute('src', newUrl);
+                    // Make sure the URL is relative
+                    if (url && !url.startsWith('http') && !url.startsWith('//')) {
+                        url = new URL(url, targetUrl).href;
+                        el.setAttribute(attr, url);
                     }
                 });
 
                 res.status(200).send(dom.serialize());
             } catch (error) {
-                res.status(500).send('Error processing HTML: ' + error.message);
+                console.error('Error processing HTML:', error);
+                res.status(500).send('Error processing HTML.');
             }
         });
     }).on('error', (e) => {
-        res.status(500).send('Error fetching URL: ' + e.message);
+        console.error('Error fetching URL:', e);
+        res.status(500).send('Error fetching URL.');
     });
 };
