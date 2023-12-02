24e2679e455fdcf41186e7faf97cd24ee089ca70
kingamada0 <152681071+kingamada0@users.noreply.github.com>
2023-12-02 07:03:23 -0800

    Update fetchContent.js

--- api/fetchContent.js
+++ api/fetchContent.js
@@ -1,5 +1,5 @@
 const https = require('https');
-const { JSDOM } = require('jsdom');
+const { JSDOM } = require('jsdom'); // Ensure JSDOM is included in your dependencies
 
 module.exports = async (req, res) => {
     const targetUrl = 'https://lordne.vercel.app/';
@@ -16,23 +16,25 @@ module.exports = async (req, res) => {
                 const dom = new JSDOM(data);
                 const document = dom.window.document;
 
-                // Append original domain to all relative URLs for link and script tags
-                const elements = document.querySelectorAll('link[href], script[src]');
-                elements.forEach(element => {
-                    const attr = element.tagName === 'LINK' ? 'href' : 'src';
-                    const url = element.getAttribute(attr);
-                    if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
-                        element.setAttribute(attr, targetUrl + url);
+                // Function to check if a URL is absolute
+                const isAbsoluteURL = (url) => /^(?:[a-z]+:)?\/\//i.test(url);
+
+                // Update href/src attributes
+                document.querySelectorAll('link[href], script[src], img[src]').forEach(element => {
+                    const url = element.getAttribute('href') || element.getAttribute('src');
+                    if (url && !isAbsoluteURL(url)) {
+                        const newUrl = new URL(url, targetUrl).href; // Construct absolute URL
+                        if (element.tagName === 'LINK') element.setAttribute('href', newUrl);
+                        if (element.tagName === 'SCRIPT' || element.tagName === 'IMG') element.setAttribute('src', newUrl);
                     }
                 });
 
-                const modifiedHtml = dom.serialize();
-                res.status(200).send(modifiedHtml);
+                res.status(200).send(dom.serialize());
             } catch (error) {
                 res.status(500).send('Error processing HTML: ' + error.message);
             }
         });
     }).on('error', (e) => {
-        res.status(500).send('Error: ' + e.message);
+        res.status(500).send('Error fetching URL: ' + e.message);
     });
 };
