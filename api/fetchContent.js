bf7f675a84f414dfc99fa1dfe9b33e502f03c23e
kingamada0 <152681071+kingamada0@users.noreply.github.com>
2023-12-02 06:31:37 -0800

    Update fetchContent.js

--- api/fetchContent.js
+++ api/fetchContent.js
@@ -1,5 +1,5 @@
 const https = require('https');
-const { JSDOM } = require('jsdom');  // Ensure JSDOM is included in your project dependencies
+const { JSDOM } = require('jsdom');
 
 module.exports = async (req, res) => {
     const targetUrl = 'https://lordne.vercel.app/';
@@ -12,40 +12,27 @@ module.exports = async (req, res) => {
         });
 
         response.on('end', () => {
-            // Parse the HTML content
-            const dom = new JSDOM(data);
-            const document = dom.window.document;
+            try {
+                const dom = new JSDOM(data);
+                const document = dom.window.document;
 
-            // Modify URLs and handle dynamic content
-            modifyUrls(document, targetUrl);
+                // Append original domain to all relative URLs for link and script tags
+                const elements = document.querySelectorAll('link[href], script[src]');
+                elements.forEach(element => {
+                    const attr = element.tagName === 'LINK' ? 'href' : 'src';
+                    const url = element.getAttribute(attr);
+                    if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
+                        element.setAttribute(attr, targetUrl + url);
+                    }
+                });
 
-            // Convert the modified DOM back to HTML
-            const modifiedHtml = dom.serialize();
-
-            res.status(200).send(modifiedHtml);
+                const modifiedHtml = dom.serialize();
+                res.status(200).send(modifiedHtml);
+            } catch (error) {
+                res.status(500).send('Error processing HTML: ' + error.message);
+            }
         });
     }).on('error', (e) => {
         res.status(500).send('Error: ' + e.message);
     });
 };
-
-function modifyUrls(document, originalDomain) {
-    // Function to check if a URL is absolute
-    const isAbsolute = (url) => url.startsWith('http://') || url.startsWith('https://');
-
-    // Modify href attributes in anchor tags and link tags
-    document.querySelectorAll('a[href], link[href]').forEach(element => {
-        if (!isAbsolute(element.href)) {
-            element.href = originalDomain + '/' + element.getAttribute('href');
-        }
-    });
-
-    // Modify src attributes in script tags and img tags
-    document.querySelectorAll('script[src], img[src]').forEach(element => {
-        if (!isAbsolute(element.src)) {
-            element.src = originalDomain + '/' + element.getAttribute('src');
-        }
-    });
-
-    // Additional logic to handle other elements or attributes as needed
-}
