9ef22ebfaae25e50dda8284db424074b2e4331b9
kingamada0 <152681071+kingamada0@users.noreply.github.com>
2023-12-02 14:53:05 -0800

    Update fetchContent.js

--- api/fetchContent.js
+++ api/fetchContent.js
@@ -1,33 +1,44 @@
 const https = require('https');
+const { JSDOM } = require('jsdom'); // Ensure JSDOM is installed
 
 module.exports = async (req, res) => {
-    const targetUrl = 'https://lordne.vercel.app/'; // Replace with your target URL
+    const targetUrl = 'https://lordne.vercel.app/';
 
     https.get(targetUrl, (response) => {
-        let data = '';
+        let rawData = '';
 
         response.on('data', (chunk) => {
-            data += chunk;
+            rawData += chunk;
         });
 
         response.on('end', () => {
-            const originalDomain = 'https://lordne.vercel.app';
+            try {
+                const dom = new JSDOM(rawData);
+                const document = dom.window.document;
 
-            // Function to check if URL is absolute
-            const isAbsoluteURL = (url) => /^(?:[a-z]+:)?\/\//i.test(url);
+                // Correctly handling base URL for relative paths
+                const baseUrl = new URL(targetUrl);
 
-            // Replace relative URLs with absolute URLs
-            data = data.replace(/(href|src|url)\("?'?\/?(.*?)"?'?\)/g, (match, p1, p2) => {
-                if (isAbsoluteURL(p2)) {
-                    return `${p1}("${p2}")`; // Return absolute URL unchanged
-                } else {
-                    return `${p1}("${originalDomain}/${p2}")`; // Prepend original domain to relative URL
-                }
-            });
+                // Modify relative URLs to absolute in <link>, <script>, and <img> tags
+                const elements = document.querySelectorAll('link[href], script[src], img[src]');
+                elements.forEach(el => {
+                    const attribute = el.tagName === 'LINK' ? 'href' : 'src';
+                    let url = el.getAttribute(attribute);
 
-            res.status(200).send(data);
+                    // Check if the URL is relative and prepend the base URL
+                    if (url && !url.startsWith('http://') && !url.startsWith('https://')) {
+                        el.setAttribute(attribute, new URL(url, baseUrl).href);
+                    }
+                });
+
+                res.status(200).send(dom.serialize());
+            } catch (error) {
+                console.error('Error processing HTML:', error);
+                res.status(500).send('Error processing HTML.');
+            }
         });
     }).on('error', (e) => {
-        res.status(500).send('Error: ' + e.message);
+        console.error('Error fetching URL:', e);
+        res.status(500).send('Error fetching URL.');
     });
 };
