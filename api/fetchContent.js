5a6330077d4281e4268ca1ede76a5c7d12bf03e8
kingamada0 <152681071+kingamada0@users.noreply.github.com>
2023-12-02 19:40:51 -0800

    Update fetchContent.js

--- api/fetchContent.js
+++ api/fetchContent.js
@@ -1,17 +1,16 @@
 const https = require('https');
+const { JSDOM } = require('jsdom');
 
 module.exports = async (req, res) => {
-    let targetUrl;
+    const baseDomain = 'https://lordne.vercel.app';
+    let targetUrl = baseDomain;
 
-    // Check if the request is for the HTML page or the JS file
+    // Check if the request is for a specific asset like a JS file
     if (req.url.includes('/script.js')) {
-        targetUrl = 'https://lordne.vercel.app/script.js';  // Path to the original JS file
-    } else {
-        targetUrl = 'https://lordne.vercel.app/';  // Path to the original HTML page
+        targetUrl += '/script.js';  // Append the specific script's path
     }
 
     https.get(targetUrl, (response) => {
-        response.setEncoding('utf8');
         let data = '';
 
         response.on('data', (chunk) => {
@@ -20,10 +19,23 @@ module.exports = async (req, res) => {
 
         response.on('end', () => {
             if (req.url.includes('/script.js')) {
-                // Set appropriate headers for JavaScript content
+                // Serve the JS file
                 res.setHeader('Content-Type', 'application/javascript');
+                res.status(200).send(data);
+            } else {
+                // Process the HTML content
+                const dom = new JSDOM(data);
+                const document = dom.window.document;
+
+                // Modify the script src to point to the serverless function
+                const scriptElements = document.querySelectorAll('script[src]');
+                scriptElements.forEach(script => {
+                    const originalSrc = script.getAttribute('src');
+                    script.setAttribute('src', `https://your-vercel-project-url/api/yourFunction${originalSrc}`);
+                });
+
+                res.status(200).send(dom.serialize());
             }
-            res.status(200).send(data);
         });
     }).on('error', (e) => {
         res.status(500).send('Error: ' + e.message);
