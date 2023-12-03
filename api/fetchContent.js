c5539ddbe1ee421e2a3837f60265ac28c4fa275a
kingamada0 <152681071+kingamada0@users.noreply.github.com>
2023-12-02 20:23:06 -0800

    Update fetchContent.js

--- api/fetchContent.js
+++ api/fetchContent.js
@@ -1,18 +1,14 @@
 const https = require('https');
-const { JSDOM } = require('jsdom');
 
 module.exports = async (req, res) => {
-    const baseDomain = 'https://lordne.vercel.app';
-    let targetUrl = baseDomain;
-
-    // Determine whether the request is for the HTML, a JS file, or a CSS file
-    if (req.url.endsWith('.js')) {
-        targetUrl += req.url;  // Append the script's path
-    } else if (req.url.endsWith('.css')) {
-        targetUrl += req.url;  // Append the CSS file's path
+    // Determine the target URL based on the request
+    let targetUrl = 'https://lordne.vercel.app';
+    if (req.url.includes('.css') || req.url.includes('.js')) {
+        targetUrl += req.url; // For CSS/JS files
     }
 
     https.get(targetUrl, (response) => {
+        let contentType = response.headers['content-type'];
         let data = '';
 
         response.on('data', (chunk) => {
@@ -20,35 +16,23 @@ module.exports = async (req, res) => {
         });
 
         response.on('end', () => {
-            if (req.url.endsWith('.js')) {
-                // Serve the JS file
-                res.setHeader('Content-Type', 'application/javascript');
-                res.status(200).send(data);
-            } else if (req.url.endsWith('.css')) {
-                // Serve the CSS file
-                res.setHeader('Content-Type', 'text/css');
-                res.status(200).send(data);
-            } else {
-                // Process the HTML content
-                const dom = new JSDOM(data);
-                const document = dom.window.document;
+            // Set the appropriate content type for the response
+            res.setHeader('Content-Type', contentType);
 
-                // Modify script src and link href to point to the serverless function
-                const assetElements = document.querySelectorAll('script[src], link[rel="stylesheet"][href]');
-                assetElements.forEach(el => {
-                    if (el.tagName === 'SCRIPT') {
-                        const originalSrc = el.getAttribute('src');
-                        el.setAttribute('src', `https://lord-test.vercel.app/api/${originalSrc}`);
-                    } else if (el.tagName === 'LINK') {
-                        const originalHref = el.getAttribute('href');
-                        el.setAttribute('href', `https://lord-test.vercel.app/api/${originalHref}`);
-                    }
-                });
-
-                res.status(200).send(dom.serialize());
+            // If it's the HTML page, modify it as needed
+            if (!req.url.includes('.css') && !req.url.includes('.js')) {
+                data = modifyHtml(data);
             }
+            
+            res.status(200).send(data);
         });
     }).on('error', (e) => {
         res.status(500).send('Error: ' + e.message);
     });
 };
+
+function modifyHtml(html) {
+    // Modify the HTML content as needed, e.g., updating asset URLs
+    // This is where you would implement your HTML modification logic
+    return html;
+}
